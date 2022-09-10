aba30d55fec14c0ca8ee577537dd3724abbaaeb6
milankarow <milan.karow@zweitag.de>
2022-09-10 13:22:24 +0200

    episode 9

--- Player.gd
+++ Player.gd
@@ -7,13 +7,17 @@ export(Resource) var moveData
 
 var velocity = Vector2.ZERO
 var state = MOVE
+var double_jump
+var buffered_jump = false
+var coyote_jump = false
 
-onready var animatedSprite = $AnimatedSprite
-onready var ladderCheck = $LadderCheck
+onready var animatedSprite: = $AnimatedSprite
+onready var ladderCheck: = $LadderCheck
+onready var jumpBufferTimer: = $JumpBufferTimer
+onready var coyoteJumpTimer: = $CoyoteJumpTimer
 
 func _ready():
 	animatedSprite.frames = load("res://PlayerGreenSkin.tres")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -35,28 +39,41 @@ func move_state(input):
 		state = CLIMB
 	
 	apply_gravity()
-	if input.x == 0:
+	
+	if input.x == 0: #check horizontal movement input
 		apply_friction()
 		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
 		animatedSprite.animation = "Run"
 		animatedSprite.flip_h = input.x > 0
-		
+
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = moveData.JUMP_SPEED
-	else:
+		reset_double_jump()
+	else: 
 		animatedSprite.animation = "Jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < moveData.MIN_JUMP_SPEED:
-			velocity.y = moveData.MIN_JUMP_SPEED
+
+	if can_jump():
+		input_jump()
+	else:
+		input_jump_release()		
+		input_double_jump()	
+		buffer_jump()
+
 	
 	var was_in_air = not is_on_floor()
+
 	velocity = move_and_slide(velocity, Vector2.UP)
 	var just_landed = is_on_floor() and was_in_air
 	if just_landed:
 		animatedSprite.animation = "Run"
 		animatedSprite.frame = 1
+	
+	var just_left_ground = not (is_on_floor() or was_in_air)
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyoteJumpTimer.start()
+		
 	
 func climb_state(input):
 	if not is_on_ladder():
@@ -66,8 +83,33 @@ func climb_state(input):
 	else:
 		animatedSprite.animation = "Idle"
 				
-	velocity = input * 50
+	velocity = input * moveData.CLIMB_SPEED
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+func input_jump():
+	if Input.is_action_just_pressed("ui_up") or buffered_jump:
+		velocity.y = moveData.JUMP_SPEED
+		buffered_jump = false
+
+func input_jump_release():
+	if Input.is_action_just_released("ui_up") and velocity.y < moveData.MIN_JUMP_SPEED:
+		velocity.y = moveData.MIN_JUMP_SPEED
+		
+func input_double_jump():
+	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+		velocity.y = moveData.JUMP_SPEED
+		double_jump -= 1
+
+func buffer_jump():
+	if Input.is_action_just_pressed("ui_up"):
+		buffered_jump = true
+		jumpBufferTimer.start()	
+
+func reset_double_jump():
+	double_jump = moveData.DOUBLE_JUMP_COUNT
+	
+func can_jump():
+	return is_on_floor() or coyote_jump
 
 func is_on_ladder():
 	if not ladderCheck.is_colliding(): return false
@@ -87,3 +129,9 @@ func apply_friction():
 		velocity.x = move_toward(velocity.x, 0, moveData.RUN_FRICTION)
 	else:	
 		velocity.x = move_toward(velocity.x, 0, moveData.AIR_FRICTION)	
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
--- PlayerMovementData.gd
+++ PlayerMovementData.gd
@@ -8,3 +8,5 @@ export(int) var MIN_JUMP_SPEED = -105
 export(int) var RUN_ACCELERATION = 10
 export(int) var RUN_FRICTION = 10
 export(int) var AIR_FRICTION = 1
+export(int) var CLIMB_SPEED = 50
+export(int) var DOUBLE_JUMP_COUNT = 1
