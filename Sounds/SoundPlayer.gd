e82104103033c7c735d781fda3fdb055052d6212
Milan Karow <milan.karow@zweitag.de>
2022-09-12 17:39:41 +0200

    episode 11e

--- Sounds/SoundPlayer.gd
+++ Sounds/SoundPlayer.gd
@@ -4,8 +4,11 @@ const HURT = preload("res://Sounds/ouch.wav")
 const JUMP1 = preload("res://Sounds/jump_1.wav")
 const JUMP2 = preload("res://Sounds/jump_2.wav")
 const LAND = preload("res://Sounds/land3.wav")
+const BGM = preload("res://Sounds/HeatleyBros - HeatleyBros VI - 8 Bit Play.mp3")
 
 onready var audioPlayers = $AudioPlayers
+onready var musicPlayer = $MusicPlayer/MusicStream
+
 
 func play_sound(sound):
 	for stream in audioPlayers.get_children():
@@ -13,3 +16,7 @@ func play_sound(sound):
 			stream.stream = sound
 			stream.play()
 			break
+
+func play_music(track):
+	musicPlayer.stream = track
+	musicPlayer.play()
--- World.gd
+++ World.gd
@@ -5,6 +5,7 @@ onready var player = $Player
 
 func _ready():
 	VisualServer.set_default_clear_color(Color.lightblue)
+	SoundPlayer.play_music(SoundPlayer.BGM)
 
 func _process(delta):
 	if player.position.y > 300: player.player_die()
