e4e224f10640a4f49ca7ba4eca14a039188ae6ce
milankarow <milan.karow@zweitag.de>
2022-09-18 11:45:22 +0200

    Moving Platform added

--- World/Door.gd
+++ World/Door.gd
@@ -6,4 +6,7 @@ export(String, FILE, "*.tscn") var target_level_path = ""
 func _on_Door_body_entered(body):
 	if not body is Player: return
 	if target_level_path.empty(): return
+	Transitions.play_exit_transition()
+	yield(Transitions, "transition_completed")
+	Transitions.play_enter_transition()
 	get_tree().change_scene(target_level_path)
